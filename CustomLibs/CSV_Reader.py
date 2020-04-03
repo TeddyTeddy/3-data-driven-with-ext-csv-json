38655a6e70fd9a9623b33bc64c26d5df2b758325
TeddyTeddy <hakan.cuzdan@yandex.com>
2020-04-03 10:02:30 +0300

    Adding a command to CSV_Reader.py

--- CustomLibs/CSV_Reader.py
+++ CustomLibs/CSV_Reader.py
@@ -6,7 +6,7 @@ class CSV_Reader:
 
     def __init__(self, csv_file_path):
         self._csvfile = open(csv_file_path, 'r')
-        self._reader = csv.reader(self._csvfile)
+        self._reader = csv.reader(self._csvfile) # an iterable
 
     def __del__(self):
         self._csvfile.close()
